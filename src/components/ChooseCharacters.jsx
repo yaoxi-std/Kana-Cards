388e5dffb7ebb8b836e8c1edba94e744c6eb9ce0
yaoxi-std <yaoxi20061225@126.com>
2025-02-22 11:44:48 +0800

    Add scroll bar, fixed mobile ui

--- src/components/ChooseCharacters.jsx
+++ src/components/ChooseCharacters.jsx
@@ -28,61 +28,64 @@ export default function ChooseCharacters({
     <>
       <div
         data-aos="slide-up"
-        className=" min-h-screen centerFlex justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-non mobile-card"
+        className="fixed inset-0 z-50 outline-none mobile-card bg-black bg-opacity-25"
       >
-        <div className="text-black max-w-lg rounded-lg overflow-hidden shadow-lg bg-white soft-shadow pt-20 pr-12 pl-12 sm:pr-12 sm:pl-12 md:pl-24 md:pr-24 pb-16">
-          <div className="flex justify-between mb-4">
-            <button
-              className="background-transparent text-sm outline-none focus:outline-none ease-linear transition-all duration-150"
-              type="button"
-              onClick={() => setIsHiraganaChart(!isHiraganaChart)}
-            >
-              {isHiraganaChart ? "Show Katakana" : "Show Hiragana"}
-            </button>
-          </div>
-          <div className="grid grid-cols-5 grid-rows-10 gap-2 gap-x-8">
-            {React.Children.toArray(chart).map((child) => {
-              if (React.isValidElement(child) && child.props.value) {
-                const kana = child.props.value;
-                return React.cloneElement(child, {
-                  onMouseDown: (e) => {
-                    e.preventDefault();
-                    setIsDragging(true);
-                    toggleCharacterInArray(kana);
-                  },
-                  onMouseEnter: (e) => {
-                    if (isDragging) {
-                      e.preventDefault();
-                      toggleCharacterInArray(kana);
-                    }
-                  },
-                });
-              }
-              return child;
-            })}
-          </div>
-          <div className="border-t border-solid border-slate-200 rounded-b p-4 mt-8">
-            <div className="flex justify-between ">
+        <div className="flex items-start justify-center p-4">
+          <div className="text-black max-w-lg rounded-lg shadow-lg bg-white soft-shadow pt-20 pr-12 pl-12 sm:pr-12 sm:pl-12 md:pl-20 md:pr-20 pb-16">
+            <div className="flex justify-between mb-4">
               <button
-                className="text-red-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none  ease-linear transition-all duration-150"
+                className="background-transparent text-sm outline-none focus:outline-none ease-linear transition-all duration-150"
                 type="button"
-                onClick={clearCharacters}
+                onClick={() => setIsHiraganaChart(!isHiraganaChart)}
               >
-                Clear
+                {isHiraganaChart ? "Show Katakana" : "Show Hiragana"}
               </button>
-              <button
-                className="text-blue-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none  ease-linear transition-all duration-150"
-                type="button"
-                data-testid="close-credits-button"
-                onClick={() => closeCharacterModal(characterArray)}
-              >
-                Confirm
-              </button>
+            </div>
+            <div className="flex flex-col">
+              <div className="grid md:grid-cols-5 gap-2 gap-x-8 max-h-[calc(100vh-20rem)] overflow-x-hidden overflow-y-auto w-full grow no-scrollbar scroll-blur">
+                {React.Children.toArray(chart).map((child) => {
+                  if (React.isValidElement(child) && child.props.value) {
+                    const kana = child.props.value;
+                    return React.cloneElement(child, {
+                      onMouseDown: (e) => {
+                        e.preventDefault();
+                        setIsDragging(true);
+                        toggleCharacterInArray(kana);
+                      },
+                      onMouseEnter: (e) => {
+                        if (isDragging) {
+                          e.preventDefault();
+                          toggleCharacterInArray(kana);
+                        }
+                      },
+                    });
+                  }
+                  return child;
+                })}
+              </div>
+              <div className="border-t border-solid border-slate-200 rounded-b p-4 mt-8">
+                <div className="flex justify-between ">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none  ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={clearCharacters}
+                  >
+                    Clear
+                  </button>
+                  <button
+                    className="text-blue-500 background-transparent font-bold uppercase text-sm outline-none focus:outline-none  ease-linear transition-all duration-150"
+                    type="button"
+                    data-testid="close-credits-button"
+                    onClick={() => closeCharacterModal(characterArray)}
+                  >
+                    Confirm
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
     </>
   );
 }
